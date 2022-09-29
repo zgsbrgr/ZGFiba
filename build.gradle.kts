12f7a6e2f14cfe73874c40fce9091a549c1fe4ca
zgsbrgr <zgsbrgr@gmail.com>
2022-09-29 16:59:59 +0200

    gitignore updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,10 @@
+buildscript {
+    dependencies {
+        classpath("com.android.tools.build:gradle:3.4.0")
+        classpath("com.google.gms:google-services:4.3.14")
+        classpath("com.google.firebase:firebase-appdistribution-gradle:3.0.3")
+    }
+}
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id ("com.android.application") version "7.2.2" apply false
