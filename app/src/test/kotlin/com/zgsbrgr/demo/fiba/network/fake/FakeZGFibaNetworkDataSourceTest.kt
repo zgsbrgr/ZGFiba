86e2c2822f90c85ca47b099f39dbb9d110d26882
zgsbrgr <zgsbrgr@gmail.com>
2022-10-10 12:11:37 +0200

    test section and roster deserialization

--- app/src/test/kotlin/com/zgsbrgr/demo/fiba/network/fake/FakeZGFibaNetworkDataSourceTest.kt
+++ app/src/test/kotlin/com/zgsbrgr/demo/fiba/network/fake/FakeZGFibaNetworkDataSourceTest.kt
@@ -1,5 +1,42 @@
 package com.zgsbrgr.demo.fiba.network.fake
 
-import org.junit.jupiter.api.Assertions.*
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.runTest
+import kotlinx.serialization.json.Json
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
 
-internal class FakeZGFibaNetworkDataSourceTest
+@OptIn(ExperimentalCoroutinesApi::class)
+class FakeZGFibaNetworkDataSourceTest {
+
+    private lateinit var subject: FakeZGFibaNetworkDataSource
+
+    private val testDispatcher = StandardTestDispatcher()
+
+    @Before
+    fun setup() {
+        subject = FakeZGFibaNetworkDataSource(
+            ioDispatcher = testDispatcher,
+            networkJson = Json { ignoreUnknownKeys = true }
+        )
+    }
+
+    @Test
+    fun testDeserializationOfMatches() = runTest(testDispatcher) {
+        Assert.assertEquals(
+            FakeDataSource.sampleSection,
+            subject.getResults().first()
+        )
+    }
+
+
+    @Test
+    fun testDeserializationOfRosters() = runTest(testDispatcher) {
+        Assert.assertEquals(
+            FakeDataSource.sampleRoster,
+            subject.getTeamRoster(null).first()
+        )
+    }
+}
