cbeb47b6843c220902eed723f79d08c5208119a1
zgsbrgr <zgsbrgr@gmail.com>
2022-10-13 13:35:43 +0200

    fixed home recyclerview bottom margin issue

--- app/src/main/kotlin/com/zgsbrgr/demo/fiba/MainActivity.kt
+++ app/src/main/kotlin/com/zgsbrgr/demo/fiba/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        WindowCompat.setDecorFitsSystemWindows(window, false)
+        //WindowCompat.setDecorFitsSystemWindows(window, false)
 
         setContentView(R.layout.activity_main)
 
