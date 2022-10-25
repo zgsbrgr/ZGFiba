d5c54d84163d42dafebced2015f8838b573158b1
zgsbrgr <zgsbrgr@gmail.com>
2022-10-25 11:44:54 +0200

    activity viewmodel where network status is monitored, viewmodels observe this

---
+++ app/src/main/kotlin/com/zgsbrgr/demo/fiba/MyActivityViewModel.kt
@@ -0,0 +1,25 @@
+@file:Suppress("MagicNumber")
+package com.zgsbrgr.demo.fiba
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.zgsbrgr.demo.fiba.data.util.NetworkMonitor
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import javax.inject.Inject
+
+@HiltViewModel
+class MyActivityViewModel @Inject constructor(
+    private val networkMonitor: NetworkMonitor
+) : ViewModel() {
+
+    val isOffline = networkMonitor.isOnline
+        .map(Boolean::not)
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000),
+            initialValue = false
+        )
+}
