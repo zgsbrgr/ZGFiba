f0ab9280ceb3786a8db8fd17e5a22c0a2e38a774
zgsbrgr <zgsbrgr@gmail.com>
2022-11-16 11:00:53 +0100

    removed active check, emit does it internally

--- app/src/main/kotlin/com/zgsbrgr/demo/fiba/data/GameInfoRepositoryImpl.kt
+++ app/src/main/kotlin/com/zgsbrgr/demo/fiba/data/GameInfoRepositoryImpl.kt
@@ -1,22 +1,20 @@
 @file:Suppress("MagicNumber")
 package com.zgsbrgr.demo.fiba.data
 
+import android.util.Log
 import com.zgsbrgr.demo.fiba.domain.MatchEvent
 import com.zgsbrgr.demo.fiba.domain.MatchEventTeam
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.currentCoroutineContext
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.isActive
 
 class GameInfoRepositoryImpl : GameInfoRepository {
 
     override fun fetchGameInfo(id: String): Flow<MatchEvent> = flow {
-
-        while (currentCoroutineContext().isActive) {
-            val randomEvent = MatchEventTeam.randomMatchEvent()
+        val randomEvent = MatchEventTeam.randomMatchEvent()
+        while (true) {
             delay(600)
             emit(
                 MatchEvent(
@@ -26,6 +24,7 @@ class GameInfoRepositoryImpl : GameInfoRepository {
                     randomEvent.iconRes
                 )
             )
+            Log.d("GI", "emitting")
         }
     }.flowOn(Dispatchers.IO)
 }
